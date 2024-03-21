fa0aeb3453e1a763530555010a721abe78f9abde
anastasia <serotonin.micro@gmail.com>
2024-03-21 14:17:10 +0100

    new UI is added
    new header, new appearance, new two raw widgets

--- cat_stuff/lib/main.dart
+++ cat_stuff/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +16,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
@@ -41,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: SizedBox(), // Replace with your NavBar widget
       ),
       body: SingleChildScrollView(
-        controller: _scrollController,
+        controller: _scrollController, // Assign ScrollController here
         physics:
             const AlwaysScrollableScrollPhysics(), // Allow smooth scrolling
         child: Center(
@@ -49,11 +48,12 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
+                height: MediaQuery.of(context).size.height + 1000,
                 child: const StackContainer(),
               ),
               ElevatedButton(
                 onPressed: () {
+                  // Ensure that the scrolling action is triggered correctly
                   _scrollController.animateTo(
                     _scrollController.position.maxScrollExtent,
                     duration: const Duration(milliseconds: 500),
@@ -71,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class StackContainer extends StatelessWidget {
-  const StackContainer({super.key});
+  const StackContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -80,29 +80,38 @@ class StackContainer extends StatelessWidget {
       height: MediaQuery.of(context).size.height,
       child: Stack(
         children: [
-          Positioned.fill(
-            child: Container(
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: const Text(
-                kLongText,
-                textAlign: TextAlign.justify,
-                style: kTextStyle,
-              ),
-            ),
-          ),
           Positioned(
-            top: 0, // Attach to upper border
-            left: 0,
+            top: 50, // Attach to upper border
+            left: 50,
             child: Container(
               width: 885, // Width specified
               height: 373, // Height specified
               decoration: BoxDecoration(border: Border.all()),
               child: const Text(
-                'Your Text Here',
+                'Anastasia\n'
+                'Lebed',
                 style: kHeaderTextStyle,
               ),
             ),
+          ),
+          Positioned(
+            top: 350, // Place under the text
+            left: 50, // Align with the left edge
+            child: Text(
+              kLongText,
+              textAlign: TextAlign.justify,
+              style: kTextStyle,
+            ),
+          ),
+          const Positioned(
+            top: 573, // Place under the text
+            left: 0, // Align with the left edge
+            child: WhiteBox(),
+          ),
+          const Positioned(
+            top: 573, // Place under the text
+            left: 600, // Place next to the first box
+            child: OrangeBox(),
           ),
         ],
       ),
@@ -110,8 +119,42 @@ class StackContainer extends StatelessWidget {
   }
 }
 
+class WhiteBox extends StatelessWidget {
+  const WhiteBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 720,
+      height: 1299,
+      child: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class OrangeBox extends StatelessWidget {
+  const OrangeBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 720,
+      height: 1299,
+      child: Container(
+        color: const Color(0xFFFF5C00),
+      ),
+    );
+  }
+}
+
 const String kLongText =
-    '/If the <time> is 0s, like the initial value, the keyframes of the animation have no effect, but the animation itself still occurs instantaneously. Specifically, start and end events are fired; if animation-fill-mode is set to backwards or both, the first frame of the animation, as defined by animation-direction, will be displayed during the animation-delay. After the animation-delay the last frame of the animation, as defined by animation-direction, will be displayed if animation-fill-mode is set to forwards or both. If animation-fill-mode is set to none the animation will have no visible effect.';
+    '/If the <time> is 0s, like the initial value, the keyframes of the animation have no effect, but the animation itself still occurs instantaneously.\n'
+    'Specifically, start and end events are fired; if animation-fill-mode is set to backwards or both, the first frame of the animation,\n'
+    'as defined by animation-direction, will be displayed during the animation-delay.\n'
+    'After the animation-delay the last frame of the animation, as defined by animation-direction, will be displayed if animation-fill-mode is set to forwards or both.\n'
+    'If animation-fill-mode is set to none the animation will have no visible effect.';
 
 const TextStyle kTextStyle = TextStyle(
   color: Colors.white,

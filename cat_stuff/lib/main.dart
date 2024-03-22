349cc7a412d846624ece738df771ce03a3222a51
anastasia <serotonin.micro@gmail.com>
2024-03-22 18:57:57 +0100

    google fonts integration
    google fonts integration and new UI

--- cat_stuff/lib/main.dart
+++ cat_stuff/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,18 +39,17 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.black,
       appBar: const PreferredSize(
         preferredSize: Size.fromHeight(30.0),
-        child: SizedBox(), // Replace with your NavBar widget
+        child: SizedBox(),
       ),
       body: SingleChildScrollView(
-        controller: _scrollController, // Assign ScrollController here
-        physics:
-            const AlwaysScrollableScrollPhysics(), // Allow smooth scrolling
+        controller: _scrollController,
+        physics: const AlwaysScrollableScrollPhysics(),
         child: Center(
           child: Column(
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height + 1000,
+                height: MediaQuery.of(context).size.height + 2000,
                 child: const StackContainer(),
               ),
             ],
@@ -79,34 +79,32 @@ class StackContainer extends StatelessWidget {
                   0.7, // Adjusted based on proportion
               decoration: BoxDecoration(border: Border.all()),
 
-              child: const Text(
+              child: Text(
                 'Anastasia\n'
                 'Lebed',
-                style: kHeaderTextStyle,
+                style: GoogleFonts.ubuntu(
+                  textStyle:
+                      const TextStyle(color: Colors.white, fontSize: 100),
+                ),
               ),
             ),
           ),
           Positioned(
-            top: MediaQuery.of(context).size.height *
-                0.35, // Place under the text
+            top: MediaQuery.of(context).size.height * 0.45,
             left: 42,
-            child: const Text(
+            child: Text(
               kLongText,
               textAlign: TextAlign.justify,
-              style: kTextStyle,
+              style: GoogleFonts.ubuntu(
+                textStyle: const TextStyle(color: Colors.white, fontSize: 25),
+              ),
             ),
           ),
           Positioned(
-            top:
-                MediaQuery.of(context).size.width * 0.7, // Place under the text
-            left: 0, // Align with the left edge
+            top: MediaQuery.of(context).size.width * 0.7,
+            left: 0,
             child: const WhiteBox(),
           ),
-          // const Positioned(
-          //   top: 573, // Place under the text
-          //   left: 600, // Place next to the first box
-          //   child: OrangeBox(),
-          // ),
         ],
       ),
     );
@@ -119,54 +117,44 @@ class WhiteBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width:
-          MediaQuery.of(context).size.width * 2, // Adjusted based on proportion
-      height: MediaQuery.of(context).size.height *
-          2, // Adjusted based on proportion
+      width: MediaQuery.of(context).size.width * 2,
+      height: MediaQuery.of(context).size.height * 2,
       child: Stack(
         children: [
           Container(
             color: Colors.white,
           ),
-          const Positioned(
-            top: 30,
-            left: 10,
+          Positioned(
+            top: 25,
+            left: 5,
             child: SizedBox(
               width: 622,
               height: 127,
               child: Text(
                 '  /coding',
-                style: TextStyle(
-                  fontFamily: 'Ubuntu',
-                  fontSize: 60, // Adjusted font size to match the requirements
-                  color: Colors.black,
-                  decoration: TextDecoration.none,
+                style: GoogleFonts.ubuntu(
+                  textStyle:
+                      const TextStyle(color: Colors.black, fontSize: 100),
                 ),
               ),
             ),
           ),
           Positioned(
-            top: 127, // Below the first text
+            top: 140,
             left: 0,
             child: SizedBox(
-              width: 607, // Adjusted width to match the specified parameters
-              height: 874, // Adjusted height to match the specified parameters
+              width: 800,
+              height: 874,
               child: Container(
-                width: 607,
-                height: 874,
-                decoration: const BoxDecoration(),
-                child: const SingleChildScrollView(
-                  // Add a SingleChildScrollView to ensure scrolling if text overflows
+                decoration: BoxDecoration(),
+                child: SingleChildScrollView(
                   child: Padding(
-                    padding: EdgeInsets.all(
-                        30.0), // Add padding of 10 pixels on each side
+                    padding: EdgeInsets.all(30.0),
                     child: Text(
                       kLongText,
-                      style: TextStyle(
-                        fontFamily: 'Ubuntu',
-                        fontSize: 20,
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
+                      style: GoogleFonts.ubuntu(
+                        textStyle:
+                            const TextStyle(color: Colors.black, fontSize: 25),
                       ),
                     ),
                   ),
@@ -179,91 +167,6 @@ class WhiteBox extends StatelessWidget {
     );
   }
 }
-
-// class OrangeBox extends StatelessWidget {
-//   const OrangeBox({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width:
-//           MediaQuery.of(context).size.width * 2, // Adjusted based on proportion
-//       height: MediaQuery.of(context).size.height *
-//           2, // Adjusted based on proportion
-
-//       child: Stack(
-//         children: [
-//           Container(
-//             color: const Color(0xFFFF5C00),
-//             child: Positioned(
-//               top: 0, // Add top position
-//               left: MediaQuery.of(context).size.width / 2 - 607 / 2,
-//               child: SizedBox(
-//                 width: MediaQuery.of(context).size.width *
-//                     2, // Adjusted width to match the specified parameters
-//                 height: MediaQuery.of(context).size.width *
-//                     2, // Adjusted height to match the specified parameters
-//               ),
-//             ),
-//           ),
-//           const Positioned(
-//             top: 30,
-//             left: 10,
-//             child: SizedBox(
-//               width: 622,
-//               height: 127,
-//               child: Text(
-//                 '  /design',
-//                 style: TextStyle(
-//                   fontFamily: 'Ubuntu',
-//                   fontSize: 60, // Adjusted font size to match the requirements
-//                   color: Colors.white,
-//                   decoration: TextDecoration.none,
-//                 ),
-//               ),
-//             ),
-//           ),
-//           Positioned(
-//             top: 127, // Below the first text
-//             left: 10, // Centered horizontally
-//             child: SizedBox(
-//               width: 607, // Adjusted width to match the specified parameters
-//               height: 874, // Adjusted height to match the specified parameters
-//               child: Container(
-//                 width: 607,
-//                 height: 874,
-//                 decoration: const BoxDecoration(),
-//                 child: const SingleChildScrollView(
-//                   // Add a SingleChildScrollView to ensure scrolling if text overflows
-//                   child: Padding(
-//                     padding: EdgeInsets.all(
-//                         30.0), // Add padding of 10 pixels on each side
-//                     child: Text(
-//                       kLongText,
-//                       style: TextStyle(
-//                         fontFamily: 'Ubuntu',
-//                         fontSize: 20,
-//                         color: Colors.white,
-//                         decoration: TextDecoration.none,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// const String kLongText =
-//     '/If the <time> is 0s, like the initial value, the keyframes of the animation have no effect, but the animation itself still occurs instantaneously.\n'
-//     'Specifically, start and end events are fired; if animation-fill-mode is set to backwards or both, the first frame of the animation,\n'
-//     'as defined by animation-direction, will be displayed during the animation-delay.\n'
-//     'After the animation-delay the last frame of the animation, as defined by animation-direction, will be displayed if animation-fill-mode is set to forwards or both.\n'
-//     'If animation-fill-mode is set to none the animation will have no visible effect.';
 
 const String kLongText =
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n'
@@ -278,11 +181,10 @@ const String kLongText =
 const TextStyle kTextStyle = TextStyle(
   color: Colors.white,
   fontFamily: 'Ubuntu',
-  fontSize: 20,
+  fontSize: 25,
 );
 
 const TextStyle kHeaderTextStyle = TextStyle(
-  fontFamily: 'Ubuntu',
-  fontSize: 70,
+  fontSize: 100,
   color: Colors.white,
 );
